f3288c146f322be38ca354d7290e93f07ad699a5
CedrickMQ <cedrick.marcial25@unach.mx>
2023-03-14 15:43:46 -0600

    micro

---
+++ fakerServicio.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from faker import Faker
+
+app = Flask(__name__)
+faker = Faker()
+
+@app.route('/crear registro')
+def Create_profiles():
+    try:
+        return {"message":"Creando Registro"}
+    except:
+        return {"Error en crear registro"}
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)

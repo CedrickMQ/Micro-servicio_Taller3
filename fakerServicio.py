d4e0dbac94155a0d0b1fa74513333c9337464c16
CedrickMQ <cedrick.marcial25@unach.mx>
2023-03-16 20:49:32 -0600

    1.2.0

--- fakerServicio.py
+++ fakerServicio.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify
 from faker import Faker
+import itertools
 
 app = Flask(__name__)
 faker = Faker()
 
+def random_data():
+    profiles = [dict(itertools.islice(faker.profile().items(),6)) for data in range (13)]
+    return profiles
+
 @app.route('/crear registro')
 def Create_profiles():
     try:
+        data = random_data()
+        return jsonify(data)
+        print(data)
         return {"message":"Creando Registro"}
     except:
         return {"Error en crear registro"}
